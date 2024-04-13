a653d922cabe733dbbd0a4db4cbe773bea11908e
fernando2134 <feremaord@gmail.com>
2024-04-13 02:29:54 -0300

    Ejercicio 4: Factorial de un número

---
+++ src/ar/edu/unju/fi/ejercicio4/Main.java
@@ -0,0 +1,34 @@
+package ar.edu.unju.fi.ejercicio4;
+
+import java.util.Scanner;
+public class Main {
+    /**
+     * @author Fernando Ordoñez
+     * @since 13-04-2024
+     *
+     * Ejercicio 4: Factorial de un número: num!
+     *
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un número entre 0 y 10: ");
+        int num = scanner.nextInt();
+
+        if (num < 0 || num > 10) {
+            System.out.println("Número fuera del rango, debe estar entre 0 y 10.");
+        } else {
+
+            int fact = 1;
+            int cont = num;
+
+            while (cont > 0) {
+                fact *= cont;
+                cont--;
+            }
+
+            System.out.println("El factorial de " + num + " es: " + fact);
+        }
+
+        scanner.close();
+    }
+}
