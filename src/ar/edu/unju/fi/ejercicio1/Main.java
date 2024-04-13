e39f582a73e7e64bfa2a826b10c8c0844d23a3e9
fernando2134 <feremaord@gmail.com>
2024-04-13 00:36:30 -0300

    Ejercicio 1: Promedio

--- src/ar/edu/unju/fi/ejercicio1/Main.java
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -1,7 +1,21 @@
 package ar.edu.unju.fi.ejercicio1;
 
 public class Main {
+    /**
+     * @author Fernando Ordoñez
+     * @since 13-04-2024
+     *
+     * Ejercicio 1: Promedio
+     *
+     */
     public static void main(String[] args) {
-       
+        int num1, num2, num3, num4, num5;
+        num1 = 8;
+        num2 = 5;
+        num3 = 1;
+        num4 = 20;
+        num5 = 9;
+        double promedio = (num1 + num2 + num3 + num4 + num5) / 5.0;
+        System.out.println("El promedio es: " + promedio);
     }
 }
